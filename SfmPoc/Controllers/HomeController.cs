192c81a85f7d3146564a739681fbc16a2d023dab
Rune Andreas Grimstad <rune@udelt.no>
2020-09-15 16:25:43 +0200

    Added more comments

--- SfmPoc/Controllers/HomeController.cs
+++ SfmPoc/Controllers/HomeController.cs
@@ -14,6 +14,8 @@ namespace SfmPoc.Controllers
 {
     public class HomeController : Controller
     {
+        // This is the front end application. 
+
         private readonly ILogger<HomeController> _logger;
         private readonly IMemoryCache _memoryCache;
 
@@ -25,6 +27,10 @@ namespace SfmPoc.Controllers
 
         public IActionResult Index()
         {
+            // This page is open for anonymous access
+            // If the user is logged on we will display information about the user 
+            // The access token is displayed here, but it would normally not be exposed to the browser
+
             ViewBag.AccessToken = GetCurrentAccessToken();
             return View(User.Claims.ToList());
         }
@@ -37,16 +43,17 @@ namespace SfmPoc.Controllers
         [Authorize(policy: "HasSession")]
         public async Task<IActionResult> Secured()
         {
+            // This page is only available if the visitor has the right cookie set and that cookie points to a valid user session
+            // Here we use the access token in the back end to call an external api and returns the result to the browser.
+            // We print out the access token, but we should not do taht in a real application
+
+            // Retrieves the access token for the current user session
+            // If there is no user session we could choose to fail or maybe tell the user they must log on
             var accessToken = GetCurrentAccessToken();
             ViewBag.AccessToken = accessToken;
 
-            var client = new HttpClient();
-            client.SetBearerToken(accessToken);
-
-            var apiUrl = $"{Request.Scheme}://{Request.Host.ToUriComponent()}/api/sample";
-
-            var result = await client.PostAsync(apiUrl, new StringContent(""));
-            var apiResult = await result.Content.ReadAsStringAsync();
+            // Calls the external api using the access token
+            var apiResult = await CallExternalApi(accessToken);
 
             return View("Secured", apiResult);
         }
@@ -57,14 +64,35 @@ namespace SfmPoc.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        private async Task<string> CallExternalApi(string accessToken)
+        {
+            // Calls an external api using the access token as a bearer token to gain access
+
+            var client = new HttpClient();
+            client.SetBearerToken(accessToken);
+
+            // Just a fancy way of determining the url of the api... 
+            // The api is not really external in this case, but you get the point!
+            var apiUrl = $"{Request.Scheme}://{Request.Host.ToUriComponent()}/api/sample";
+
+            var result = await client.PostAsync(apiUrl, new StringContent(""));
+            var apiResult = await result.Content.ReadAsStringAsync();
+
+            return apiResult;
+        }
+
         private string GetAccessTokenForSession(string sessionId)
         {
+            // Gets the access token that belongs to the given session
             var accessToken = _memoryCache.Get<string>(sessionId + "_at");
             return accessToken;
         }
 
         private string GetCurrentAccessToken()
         {
+            // Gets the session id from the cookie via the claims principal
+            // Then gets the access token for that session id
+
             var sessionId = User.FindFirstValue("SessionId");
             if (string.IsNullOrEmpty(sessionId))
             {
--- SfmPoc/Startup.cs
+++ SfmPoc/Startup.cs
@@ -16,22 +16,29 @@ namespace SfmPoc
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
 
             services
                 .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(options => {
+                .AddCookie(options =>
+                {
+                    // Configures the cookie auhtentication so that we don't try to logon the user
+                    // in the traditional way. This would probably point to a page that explains to the 
+                    // visito what they need to do
                     options.LoginPath = "/home/accessdenied";
-                    options.AccessDeniedPath = "/home/accessdenied";                
+                    options.AccessDeniedPath = "/home/accessdenied";
                 });
 
-            services.AddAuthorization(options => {
+            services.AddAuthorization(options =>
+            {
+                // Sets up a policy that requires the user to have a session id.
+                // This policy is used on all protected resources of the application
                 options.AddPolicy("HasSession", p => p.RequireClaim("SessionId"));
             });
 
+            // Instead of using a database we use a memory cache for simplicity
             services.AddMemoryCache();
         }
 
@@ -48,6 +55,7 @@ namespace SfmPoc
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
